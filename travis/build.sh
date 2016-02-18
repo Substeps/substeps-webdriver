19ff18797d30020069ee3a0705731530c0e0e311
Ian Moore <ian@itmoore.co.uk>
2016-02-18 20:56:55 +0000

    more travis tweaks

--- travis/build.sh
+++ travis/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH == 'master'] && [$TRAVIS_REPO_SLUG == 'Substeps/substeps-webdriver']; then
+if [[ $TRAVIS_BRANCH == 'master' ]] && [[ $TRAVIS_REPO_SLUG == 'Substeps/substeps-webdriver' ]]; then
    mvn deploy --settings travis/settings.xml
 else
   mvn verify
