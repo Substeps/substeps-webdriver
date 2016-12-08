fad074157dc8b3992d2930a9d1082729bc78568b
Ian Moore <ian@itmoore.co.uk>
2016-12-08 21:37:40 +0000

    attempt to run chrome headless on travis

--- travis/build.sh
+++ travis/build.sh
@@ -5,5 +5,5 @@ echo "Building branch: $TRAVIS_BRANCH on slug: $TRAVIS_REPO_SLUG is pull request
 if [[ "${TRAVIS_PULL_REQUEST}" = "false"  ]] && [[ $TRAVIS_BRANCH == 'master' ]] && [[ $TRAVIS_REPO_SLUG == 'Substeps/substeps-webdriver' ]]; then
    mvn deploy --settings travis/settings.xml
 else
-  mvn verify
+  mvn verify -Denvironment=travis
 fi
