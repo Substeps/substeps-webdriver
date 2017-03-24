8e91a8e575045c502e2446c5e09738b07b0111df
Ian Moore <ian@itmoore.co.uk>
2017-03-24 08:08:10 +0000

    removed non fatal tags, tweaked travis config for snapshot builds

--- travis/build.sh
+++ travis/build.sh
@@ -3,7 +3,7 @@
 echo "Building branch: $TRAVIS_BRANCH on slug: $TRAVIS_REPO_SLUG is pull request: $TRAVIS_PULL_REQUEST"
 
 if [[ "${TRAVIS_PULL_REQUEST}" = "false"  ]] && [[ $TRAVIS_BRANCH == 'master' ]] && [[ $TRAVIS_REPO_SLUG == 'Substeps/substeps-webdriver' ]]; then
-   mvn deploy --settings travis/settings.xml
+   mvn deploy --settings travis/settings.xml -Pself-test
 else
   mvn verify -Denvironment=travis -Pself-test
 fi
